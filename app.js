e39cb6cf2983b2550ede56ad8aa95c9db4ceb029
Blairon <blairon1@my.yorku.ca>
2026-06-13 19:07:34 -0400

    Feat: Implemented basic addition with two numbers for one time calculation

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
+let firstNumber = null;
+let secondNumber = null;
+
+let lastOperator = null;
+let operator = null;
+
+let currentNumber = "";
+
 
 
 //       ----------------       DOM manipulation    ------------------  //
@@ -7,12 +15,17 @@ const display = document.querySelector('.display'); // display reference
 const numButton = document.querySelectorAll('.num'); // num-button reference
 numButton.forEach(element => {
     element.addEventListener("click", ()=>{
-        //console.log("Clicked");
         let numDisplay = element.textContent; //Selected number for the display
+        updateCurrentNumber(numDisplay);
+        console.log(`Clicked| Current Number: ${currentNumber}`);
+        updateDisplay(currentNumber);
+
+
    })
 });
 
 
+//       ----------------       Clear and Equals expressions    ------------------  //
 
 const clearButton = document.querySelector('#clear'); // clear-button reference
 clearButton.addEventListener("click", ()=>{
@@ -21,7 +34,12 @@ clearButton.addEventListener("click", ()=>{
 
 
 const evaluate = document.querySelector('#equal'); // equal-button reference
-evaluate.addEventListener("click", evaluateExpression);
+evaluate.addEventListener("click", () =>{
+   if(firstNumber != null && operator != null && currentNumber.length > 0){
+    secondNumber = Number(currentNumber);
+    operate(firstNumber,operator,secondNumber);
+   }
+});
 
 
 
@@ -30,14 +48,31 @@ evaluate.addEventListener("click", evaluateExpression);
 
 const addButton = document.querySelector('#addition');
 addButton.addEventListener("click", ()=>{
-   
-
+    if(currentNumber.length > 0 && firstNumber == null && secondNumber == null){
+        updateDisplay(currentNumber + " +");
+        firstNumber = Number(currentNumber);
+        currentNumber = "";
+    }
+    operator = "+";
 });
 
 
 const subButton = document.querySelector('#sub');
 subButton.addEventListener("click", ()=>{
-    
+    operator = "-";
+
+});
+
+const multButton = document.querySelector('#mult');
+multButton.addEventListener("click", ()=>{
+   operator = "x";
+
+});
+
+
+const divideButton = document.querySelector('#divide');
+divideButton.addEventListener("click", ()=>{
+    operator = "/";
 
 });
 
@@ -49,6 +84,9 @@ function updateDisplay(newDisplay){
     display.textContent = newDisplay;
 }
 
+function updateCurrentNumber(newNumber){
+    currentNumber += newNumber;
+}
 
 
 // Function operations
@@ -70,7 +108,9 @@ function divide(number1, number2){
 
 
 // Method to evaluate expressions based on
-function evaluateExpression(number1, operation, number2){
-
-
+function operate(number1, operation, number2){
+    if(operation == "+"){
+        firstNumber = add(number1,number2);
+        updateDisplay(firstNumber);
+    }
 }
